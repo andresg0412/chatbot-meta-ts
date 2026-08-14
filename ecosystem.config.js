c57b13afd32cc0553814ce645d3b0ec491096164
German Andres Gamboa Portilla <ggamboa@coordinadora.com>
2026-08-13 19:59:35 -0500

    fix: error en build

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,14 +2,17 @@ module.exports = {
   apps: [
     {
       name: 'bot-meta',
-      script: './dist/app.js',
+      script: './src/app.ts',
+      interpreter: './node_modules/.bin/tsx',
       instances: 1,
       autorestart: true,
       watch: false,
       max_restarts: 10,
       restart_delay: 3000,
-      node_args: '--max-old-space-size=4096',
       max_memory_restart: '1G',
+      env: {
+        NODE_OPTIONS: '--max-old-space-size=4096',
+      },
     },
   ],
 };
